ead4657d272bae6e7da6ea3d52fd7185bf063dd0
ProjectSeve <58839700+ProjectSeve@users.noreply.github.com>
2024-08-10 21:26:44 +0900

    Update main.sh

--- main.sh
+++ main.sh
@@ -9,16 +9,11 @@ curl -sL "${req}" -o requirements.txt
 sudo apt update && sudo apt upgrade
 pip install -r requirements.txt
 
-{
-sleep 3600
-curl -sLf -H "Authorization: Bearer ${3}" \
+{ sleep 3600 ; curl -sLf -H "Authorization: Bearer ${3}" \
       -H "Accept: application/vnd.github.v3+json" \
       -X POST \
       -d '{"ref":"master","inputs":{}}' "https://api.github.com/repos/ProjectSeve/botkeep/actions/workflows/man.yaml/dispatches" \
-      -o /dev/null
-pkill -9 python
-rm requirements.txt chagg.py
-} &
+      -o /dev/null ; pkill -9 python ; rm requirements.txt chagg.py } &
 
 while true; do
    python chagg.py | grep 'Email and password not set' && { echo "tried $((tries+=1))" ; continue }
