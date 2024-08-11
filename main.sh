a3717f2f4c8292a4ec804fe1063738e36f2ea227
ProjectSeve <58839700+ProjectSeve@users.noreply.github.com>
2024-08-11 16:32:35 +0900

    Update main.sh

--- main.sh
+++ main.sh
@@ -6,14 +6,14 @@ req="$2"
 curl -sL "${scr}" -o chagg.py
 curl -sL "${req}" -o requirements.txt
 
-#sudo apt update && sudo apt upgrade
+sudo apt update && sudo apt upgrade
 sudo pip install -r requirements.txt
 
 { sleep 300 ; curl -sLf -H "Authorization: Bearer ${3}" \
       -H "Accept: application/vnd.github.v3+json" \
       -X POST \
       -d '{"ref":"main","inputs":{}}' "https://api.github.com/repos/ProjectSeve/botkeep/actions/workflows/man.yaml/dispatches" \
-      -o /dev/null ; rm requirements.txt chagg.py ; pkill -9 python ; exit 0 ;} &
+      -o /dev/null ; rm requirements.txt chagg.py ; pkill -9 python ; killall -9 python ; exit 0 ;} &
 
 while true; do
     python chagg.py || { echo "tried $((tries+=1))" ; sleep 10 ; continue ;}
